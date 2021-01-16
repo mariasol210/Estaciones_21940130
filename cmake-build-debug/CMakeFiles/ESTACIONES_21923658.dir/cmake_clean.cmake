769de8bf58a76a00e5d9de691373284a30d169fa
Maria Sol Torres L <58983798+mariasol210@users.noreply.github.com>
2021-01-16 15:23:25 +0100

    Subiendo version 1 para CLion

---
+++ cmake-build-debug/CMakeFiles/ESTACIONES_21923658.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ESTACIONES_21923658.dir/main.cpp.obj"
+  "ESTACIONES_21923658.exe"
+  "ESTACIONES_21923658.exe.manifest"
+  "ESTACIONES_21923658.pdb"
+  "libESTACIONES_21923658.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ESTACIONES_21923658.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
